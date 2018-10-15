fadfffbd4550d6c909a58dd3b65a63877f2ce452
ervanSTRI <RutishauserE@si.edu>
2018-10-15 13:14:13 +0200

    Re-send AGB functions (that works)

--- R/AGBfluxes_functions.R
+++ R/AGBfluxes_functions.R
@@ -238,7 +238,7 @@ data_preparation <- function(site,
 #'
 #' @keywords internal
 #' @noRd
-consolidate_data <- function(df, taper_correction, fill_missing, stem) {
+consolidate_data <- function(df, dbh_units,taper_correction, fill_missing, stem) {
   if (stem) {
     df[, "id" := paste(treeID, stemID, sep = "-")] # creat a unique tree-stem ID
     df <- df[order(id, CensusID)]
@@ -1284,4 +1284,16 @@ trim_growth <- function(cens,
   growth[!accept] <- NA
 
   growth
+}
+
+#' Create a dataframe with one row full of missing values.
+#'
+#' @param .names String giving the names of the dataframe to create.
+#'
+#' @return A dataframe.
+#' @keywords internal
+#' @noRd
+receiving_df <- function(.names) {
+  na <- as.list(rep(NA, length(.names)))
+  stats::setNames(data.frame(na), .names)
 }
