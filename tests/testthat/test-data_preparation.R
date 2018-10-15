b8d3b2c54f06f5e6364d06fe3f96f8c93b14bddf
maurolepore <maurolepore@gmail.com>
2018-10-15 19:58:46 +0000

    Test for known output and expected data structures. Closes #17.

---
+++ tests/testthat/test-data_preparation.R
@@ -0,0 +1,38 @@
+context("data_preparation")
+
+prep <- data_preparation(
+  site = "barro colorado island",
+  stem = TRUE,
+  taper_correction = TRUE,
+  fill_missing = TRUE,
+  use_palm_allometry = TRUE,
+  flag_stranglers = TRUE,
+  dbh_stranglers = 500,
+  maxrel = 0.2,
+  write_errors_to = NULL,
+  DATA_path = NULL,
+  exclude_interval = NULL,
+  graph_problems_to = NULL
+)
+
+describe("data_preparation", {
+  it("outputs known output", {
+    expect_known_output(
+      as.data.frame(prep), "ref-data_preparation", update = FALSE, print = TRUE
+    )
+  })
+
+  it("outputs expected structure", {
+    expect_is(prep, "data.table")
+    expect_is(prep, "data.frame")
+
+    nms <- c(
+      "treeID", "dbh1", "dbhc1", "status1", "code1", "hom1", "sp", "wsg",
+      "agb1", "date1", "dbh2", "dbhc2", "status2", "code2", "hom2", "agb2",
+      "date2", "broken", "agbl", "agb1.surv", "interval", "year", "gx", "gy",
+      "quadrat", "name", "ID", "int", "code", "dHOM", "prod.g", "prod.r",
+      "loss", "ficus", "prod.rel", "error", "error.loss"
+    )
+    expect_named(prep, nms)
+  })
+})
